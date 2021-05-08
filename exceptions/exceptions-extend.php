89e25517917687cf21c2f97c30c8fbdd464631cc
armando.nascimento@gsurfnet.net <Jr+180681>
2021-05-08 11:33:38 -0300

    Extend PHP exception

---
+++ exceptions/exceptions-extend.php
@@ -0,0 +1,36 @@
+<?php
+class InvalidCCNumberException extends InvalidArgumentException
+{
+    public function __construct($message = 'Other Error', $code = 0, $previous = null)
+    {
+        return parent::__construct($message, $code, $previous);
+    }
+}
+
+try {
+    processCC(1);
+} catch (InvalidCCNumberException $e) {
+    /** Throw Exception */
+    echo $e->getMessage() . PHP_EOL;
+    echo get_class($e) . PHP_EOL;
+}
+
+function processCC($numb = null, $zipCode = null)
+{
+    try {
+        validate($numb, $zipCode);
+    } catch (Exception $e) {
+        throw $e;
+    }
+    echo 'processed';
+}
+
+function validate($numb, $zipCode)
+{
+    if (is_null($numb)) {
+        throw new InvalidCCNumberException('No CC Number');
+    }
+    if (is_null($zipCode)) {
+        throw new InvalidCCNumberException('No ZipCode');
+    }
+}
