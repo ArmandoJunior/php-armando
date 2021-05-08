feaa47e053b6ca62e90ace219867ae455ce30959
armando.nascimento@gsurfnet.net <Jr+180681>
2021-05-08 11:05:25 -0300

    PHP SPL exception

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -4,8 +4,11 @@ try {
 } catch (Exception $e) {
     /** Throw Exception */
     echo $e->getMessage() . PHP_EOL;
+    echo get_class($e) . PHP_EOL;
+
     /** Nested exceptions */
     echo $e->getPrevious()->getMessage();
+    echo get_class($e->getPrevious()) . PHP_EOL;
 }
 
 function processCC($numb = null, $zipCode = null)
@@ -13,7 +16,7 @@ function processCC($numb = null, $zipCode = null)
     try {
         validate($numb, $zipCode);
     } catch (Exception $e) {
-        throw new Exception('Invalid Inputs', null, $e);
+        throw new BadFunctionCallException('Invalid Inputs', null, $e);
     }
     echo 'processed';
 }
@@ -21,6 +24,6 @@ function processCC($numb = null, $zipCode = null)
 function validate($numb, $zipCode)
 {
     if (is_null($numb)) {
-        throw new Exception('No CC Number' . PHP_EOL);
+        throw new InvalidArgumentException('No CC Number' . PHP_EOL);
     }
 }
